2108fc0763130a4c7d35f94672a91580cc6756d3
kkafi09 <kafi.dev27@gmail.com>
2023-10-13 09:33:31 +0700

    fix: eslint deps

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,37 +1,37 @@
 import {
-  Controller,
-  Post,
-  Body,
-  Res,
-  Req,
-  Get,
-  UseGuards,
+    Body,
+    Controller,
+    Get,
+    Post,
+    Req,
+    Res,
+    UseGuards,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+import { AuthGuard } from '../guards/auth.guard';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { Response, Request } from 'express';
-import { AuthGuard } from '../guards/auth.guard';
-import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('AUTH')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-  @Get('/user')
-  @UseGuards(AuthGuard)
-  getUserAuth(
-    @Req() request: Request,
-    @Res() response: Response,
-  ): Promise<any> {
-    return this.authService.getUserAuth(request, response);
-  }
+    constructor(private readonly authService: AuthService) {}
+    @Get('/user')
+    @UseGuards(AuthGuard)
+    getUserAuth(
+        @Req() request: Request,
+        @Res() response: Response,
+    ): Promise<any> {
+        return this.authService.getUserAuth(request, response);
+    }
 
-  @Post()
-  create(
-    @Body() createAuthDto: CreateAuthDto,
-    @Req() request: Request,
-    @Res() response: Response,
-  ) {
-    return this.authService.signIn(createAuthDto, request, response);
-  }
+    @Post()
+    create(
+        @Body() createAuthDto: CreateAuthDto,
+        @Req() request: Request,
+        @Res() response: Response,
+    ): Promise<any> {
+        return this.authService.signIn(createAuthDto, request, response);
+    }
 }
